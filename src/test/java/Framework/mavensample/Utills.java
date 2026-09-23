e717252fa6a1820dd8c4afd442878d3e3a819a46
Admin <arun31190@gmail.com>
2026-09-23 23:38:37 +0530

    sec commit

--- src/test/java/Framework/mavensample/Utills.java
+++ src/test/java/Framework/mavensample/Utills.java
@@ -151,7 +151,9 @@ public class Utills {
         System.out.println("Failed to save screenshot: " + e.getMessage());
     } finally {
         driver.quit();
+       // driver.quit();
         
+        // driver.quit();
     }
 		
 	}
